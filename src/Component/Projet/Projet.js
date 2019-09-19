bf7de84fc986c72fa71e6294abed5449b834529c
bmontbe <bdemontbeillard@gmail.com>
2019-09-19 21:23:30 +0200

    fix

--- src/Component/Projet/Projet.js
+++ src/Component/Projet/Projet.js
@@ -37,61 +37,61 @@ const totalApport = () => {
      <div>{props.nomProjet ? `Récapitulatif de mon projet : ${props.nomProjet}` : "Récapitulatif de mon projet"}</div>
     </div>
    <div className="categorie">
-     <div className="title">Montant de l'acquisition : </div>
+     <div className="title">Montant de l'acquisition</div>
      <div className="montant"><span>{numStr(props.montantAcquisition)}</span> euros</div>
    </div>
    <div className="categorie">
-     <div className="title">Montant des travaux : </div>
+     <div className="title">Montant des travaux</div>
      <div className="montant"><span>{numStr(props.montantTravaux)}</span> euros</div>
     </div>
     <div className="categorie">
-     <div className="title">Frais de notaire : </div>
+     <div className="title">Frais de notaire</div>
      <div className="montant"><span>{numStr(Number(props.fraisNotaire).toFixed(0))}</span> euros</div>
     </div>
     <div className="categorie">
-     <div className="title">Frais d'agence : </div>
+     <div className="title">Frais d'agence</div>
      <div className="montant"><span>{numStr(Number(props.fraisAgence).toFixed(0))}</span> euros</div>
     </div>
     <div className="categorie">
-     <div className="title">Frais de garanties : </div>
+     <div className="title">Frais de garanties</div>
      <div className="montant"><span>{numStr(Number(props.garantie))}</span> euros</div>
     </div>
     <div className="categorie">
-     <div className="title">Frais de dossier : </div>
+     <div className="title">Frais de dossier</div>
      <div className="montant"><span>{numStr(Number(props.fraisDossier))}</span> euros</div>
     </div>
     <div className="categorieTotal">
-     <div className="title">Total du projet : </div>
+     <div className="title">Total du projet</div>
      <div className="montant"><span>{totalProjet()}</span> euros</div>
     </div>
     <div className="section">
-     <div>Mes Revenus et mes charges annuels</div>
+     <div>Mes Revenus et mes charges mensuels</div>
     </div>
     <div className="categorie">
-     <div className="title">Total des Revenus : </div>
+     <div className="title">Total des Revenus</div>
      <div className="montant"><span>{numStr(props.revenus)}</span> euros</div>
     </div>
     <div className="categorie">
-     <div className="title">Total de mes charges : </div>
+     <div className="title">Total de mes charges</div>
      <div className="montant"><span>{numStr(props.charges)}</span> euros</div>
     </div>
     <div className="section">
      <div>Mon épargne et mon apport</div>
     </div>
     <div className="categorie">
-     <div className="title">Mon épargne : </div>
+     <div className="title">Mon épargne</div>
      <div className="montant"><span>{numStr(props.epargne)}</span> euros</div>
     </div>
     <div className="categorie">
-     <div className="title">Mon apport : </div>
+     <div className="title">Mon apport</div>
      <div className="montant"><span>{numStr(props.apport)}</span> euros</div>
     </div>
     <div className="categorie">
-     <div className="title">Apport supplémentaire : </div>
+     <div className="title">Apport supplémentaire</div>
      <div className="montant"><span>{numStr(props.apportSup)}</span> euros</div>
     </div>
     <div className="categorieTotal">
-     <div className="title">Total de votre apport : </div>
+     <div className="title">Total de votre apport</div>
      <div className="montant"><span>{totalApport()}</span> euros</div>
     </div>
  </div>
