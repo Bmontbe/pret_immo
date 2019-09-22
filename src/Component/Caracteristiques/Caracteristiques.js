19a4ca41c09840500c9118d79d5a8e7dbf3fe1ab
bmontbe <bdemontbeillard@gmail.com>
2019-09-22 16:35:12 +0200

    optimisation caracteristiques

--- src/Component/Caracteristiques/Caracteristiques.js
+++ src/Component/Caracteristiques/Caracteristiques.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Modal, Button } from 'semantic-ui-react';
 import './Caracteristiques.css';
-import { tsOptionalType } from '@babel/types';
 import TableauAmortissement from '../TableauAmortissement/TableauAmortissement'
 
 function Caracteristiques(props) {
@@ -19,53 +17,64 @@ function Caracteristiques(props) {
   const nouvellesCharges = props.charges + parseFloat(mensualite())
 
   const endettement = () => {
-    if (props.revenus) {
+    if (mensualite() > 0) {
       return ((nouvellesCharges / props.revenus) * 100).toFixed(2)
+    } else if (mensualite() <= 0) {
+      return ((props.charges / props.revenus) * 100).toFixed(2)
     }
     else return 0
   }
 
   const rav = () => {
-    return (props.revenus - props.charges - mensualite()).toFixed(0)
+    if (props.revenus) {
+      return (props.revenus - props.charges - mensualite()).toFixed(0)
+    } return 0
   }
 
   const adi = () => {
     const MrMme = Number(props.assurancesMonsieur) + Number(props.assurancesMadame)
-    const dureePret = Number(props.duree)
-    return MrMme * dureePret
+    return MrMme
+
+  }
+
+  const totalMensualiteAdi = () => {
+    if (mensualite() > 0) {
+      const result = Number(mensualite()) + Number(adi())
+      return result.toFixed(2)
+    } return 0
   }
 
   return (
     <div>
       <div className="blockCaracteristiques">
-      <div className="sectionCaracteristiques">
-        <div className="titreCaracteristiques">Résultat de votre prêt pour votre projet</div>
-      </div>
-      <div className="caracteristiques">
-        <div className="categorieCaracteristiques">
-          <div className="title">Montant du prêt</div>
-          <div className="montant"><span>{new Intl.NumberFormat().format(montantPret)}</span> €</div>
+        <div className="sectionCaracteristiques">
+          <div className="titreCaracteristiques">Résultat de votre prêt pour votre projet</div>
         </div>
-        <div className="categorieCaracteristiques">
-          <div className="title">Mensualité</div>
-          <div className="montant"><span>{new Intl.NumberFormat().format(mensualite())}</span> €</div>
-        </div>
-        <div className="categorieCaracteristiques">
-          <div className="title">Assurances emprunteur(s) /mois</div>
-          <div className="montant"><span>{new Intl.NumberFormat().format(adi())}</span> €</div>
-        </div>
-        <div className="categorieCaracteristiques">
-          <div className="title">Taux endettement</div>
-          <div className="montant"><span>{endettement()}</span> %</div>
-        </div>
-        <div className="categorieCaracteristiques">
-          <div className="title">Reste à vivre /mois</div>
-          <div className="montant"><span>{new Intl.NumberFormat().format(rav())}</span> €</div>
-        </div>
-        <div className="categorieCaracteristiques">
-          <div className="title">Epargne restante</div>
-          <div className="montant"><span>{new Intl.NumberFormat().format(props.epargne - props.apport)}</span> €</div>
-        </div>
+        <div className="caracteristiques">
+          <div className="categorieCaracteristiques">
+            <div className="title">Montant du prêt</div>
+            <div className="montant"><span>{mensualite() > 0 ? new Intl.NumberFormat().format(montantPret) : 0}</span> €</div>
+          </div>
+          <div className="categorieCaracteristiques">
+            <div className="title">Mensualité</div>
+            <div className="montant"><span>{mensualite() > 0 ? new Intl.NumberFormat().format(mensualite()) : 0}</span> €</div>
+          </div>
+          <div className="categorieCaracteristiques">
+            <div className="title">Total /mois</div>
+            <div className="montant"><span>{new Intl.NumberFormat().format(totalMensualiteAdi())}</span> €</div>
+          </div>
+          <div className="categorieCaracteristiques">
+            <div className="title">Taux endettement</div>
+            <div className="montant"><span>{new Intl.NumberFormat().format(endettement())}</span> %</div>
+          </div>
+          <div className="categorieCaracteristiques">
+            <div className="title">Reste à vivre /mois</div>
+            <div className="montant"><span>{new Intl.NumberFormat().format(rav())}</span>€</div>
+          </div>
+          <div className="categorieCaracteristiques">
+            <div className="title">Epargne restante</div>
+            <div className="montant"> {props.epargne ? <span>{new Intl.NumberFormat().format(props.epargne - props.apport)}</span> : <span>0</span>} €</div>
+          </div>
         </div>
       </div>
       <TableauAmortissement
